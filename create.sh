b23ec6fd18ceffba0f1a8615a377fd94211267cc
Burak Sekili <buraksekili@gmail.com>
2024-06-05 18:29:05 +0300

    create OAS apis as well as classic apis
    Signed-off-by: Burak Sekili <buraksekili@gmail.com>

--- create.sh
+++ create.sh
@@ -1,12 +1,58 @@
 #!/bin/bash
+
 #
 # Creates a specified number of APIs on the Tyk Dashboard. By default, it creates 5 APIs.
-# 
-# Usage: 
-#   TYK_AUTH=<AUTH_KEY> MAX=<MAX> sh create.sh
+#
+# Usage:
+#   TYK_AUTH=<AUTH_KEY> TYK_URL=<TYK_URL> MAX=<MAX> ./create.sh
 
-createApi() {
-    reqBody='{
+createOasApi() {
+  reqBody='{
+      "info": {
+        "title": "%s",
+        "version": "1.0.0"
+      },
+      "openapi": "3.0.3",
+      "components": {},
+      "paths": {},
+      "x-tyk-api-gateway": {
+        "info": {
+          "id": "%s",
+          "name": "%s",
+          "state": {
+            "active": true
+          }
+        },
+        "upstream": {
+          "url": "https://petstore.swagger.io/v2"
+        },
+        "server": {
+          "listenPath": {
+            "value": "%s",
+            "strip": true
+          }
+        }
+      }
+    }'
+
+  reqBody=$(printf "$reqBody" "$1" "$1" "$1" "$2")
+
+  echo $reqBody
+
+  curl -sSi -H "Authorization: $TYK_AUTH" \
+    -H "Content-Type: application/json" \
+    -X POST \
+    -d "$reqBody" "$TYK_URL"/api/apis/oas
+
+
+  curl -sSi -H "Authorization: $TYK_AUTH" \
+    -H "Content-Type: application/json" \
+    -X PUT \
+    -d '{ "categories": ["oascategory"]}' "$TYK_URL"/api/apis/oas/"$1"/categories
+}
+
+createClassicApi() {
+  reqBody='{
         "api_definition": {
             "name": "%s",
             "proxy": {
@@ -30,17 +76,16 @@ createApi() {
             }
         }
     }'
-    reqBody=$(printf "$reqBody" "$1" "$2" "$2")
+  reqBody=$(printf "$reqBody" "$1" "$2" "$2")
 
-    
-    curl -sSi -H "Authorization: $TYK_AUTH" \
-        -H "Content-Type: application/json" \
-        -X POST \
-        -d "$reqBody" $TYK_URL/api/apis
+  curl -sSi -H "Authorization: $TYK_AUTH" \
+    -H "Content-Type: application/json" \
+    -X POST \
+    -d "$reqBody" $TYK_URL/api/apis
 }
 
 createPolicy() {
-    reqBody='{
+  reqBody='{
         "rate": 1000,
         "per": 60,
         "quota_max": -1,
@@ -50,34 +95,33 @@ createPolicy() {
         "name": "%s",
         "active": true
     }'
-    reqBody=$(printf "$reqBody" "$1")
+  reqBody=$(printf "$reqBody" "$1")
 
-    curl -sSi -H "Authorization: $TYK_AUTH" \
-        -H "Content-Type: application/json" \
-        -X POST \
-        -d "$reqBody" $TYK_URL/api/portal/policies/
+  curl -sSi -H "Authorization: $TYK_AUTH" \
+    -H "Content-Type: application/json" \
+    -X POST \
+    -d "$reqBody" $TYK_URL/api/portal/policies/
 }
 
-if [[ -z "$MAX" ]]
-then
-    MAX=5
+if [[ -z "$MAX" ]]; then
+  MAX=5
 fi
 
-
 RANDOM=$$
-for i in `seq 1 $MAX`
-do
-    env="#testing"
-    r=$(($RANDOM%10))
-    if [[ $r -gt 5 ]]
-    then
-	    env="#production"
-    fi
+for i in $(seq 1 $MAX); do
+  env="#testing"
+  r=$(($RANDOM % 10))
+  if [[ $r -gt 5 ]]; then
+    env="#production"
+  fi
 
-    apiName=$(printf "test-api-%d %s" $i $env)
-    listenPath=$(printf "test-api-%d" $i)
-    createApi "$apiName" "$listenPath"
+  apiName=$(printf "test-api-%d %s" $i $env)
+  listenPath=$(printf "test-api-%d" $i)
+  createClassicApi "$apiName" "$listenPath"
 
-    policyName=$(printf "custom-policy-%d" $i)
-    createPolicy $policyName
+  oasApiName=$(printf "test-api-%d" $i)
+  createOasApi "$oasApiName" "$listenPath"
+
+#  policyName=$(printf "custom-policy-%d" $i)
+#  createPolicy $policyName
 done
